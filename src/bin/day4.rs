134cfb50da1e71f8d48357e10cebd42573bc9f52
Neil Burningham <neil.burningham@mediakind.com>
2025-12-16 17:54:40 +0000

    Day 4 part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -30,15 +30,15 @@ fn day4(input: &str) -> u32 {
 }
 
 const DIRECTIONS: [(i32, i32); 8] = [
-    (-1, -1),
-    (0, -1),
-    (1, -1),
-    (-1, 0),
+    (-1, -1), // up / left
+    (0, -1),  // up
+    (1, -1),  // up / right
+    (-1, 0),  // left
     // no (0, 0) - that's us
-    (1, 0),
-    (-1, 1),
-    (0, 1),
-    (1, 1)
+    (1, 0),   // right
+    (-1, 1),  // down / left
+    (0, 1),   // down
+    (1, 1)    // down / right
 ];
 
 fn is_accessible(grid: &Vec<Vec<bool>>, x: usize, y: usize) -> bool {
@@ -66,6 +66,37 @@ fn is_accessible(grid: &Vec<Vec<bool>>, x: usize, y: usize) -> bool {
     surrounding_rolls < 4
 }
 
+fn day4b(input: &str) -> u32 {
+    let mut total_moved_rolls = 0;
+
+    let mut grid = parse(input);
+
+    // Assumes all row & columns have the same length...
+    let height: usize = grid.len();
+    let width: usize = grid[0].len();
+
+    loop {
+        let mut moved_rolls = 0;
+        for y in 0..height {
+            for x in 0..width {
+                let mut has_roll = &grid[y][x];
+                println!("FOUND {} at {}, {}", *has_roll, x, y);
+                if *has_roll && is_accessible(&grid, x, y) {
+                    grid[y][x] = false;
+                    moved_rolls += 1;
+                }
+            }
+        }
+        total_moved_rolls += moved_rolls;
+        if moved_rolls == 0 {
+            break;
+        }
+    }
+
+    total_moved_rolls
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,4 +114,19 @@ mod tests {
         let result = day4(&data);
         assert_eq!(result, 1604);
     }
+
+    #[test]
+    fn day4b_example() {
+        let data = std::fs::read_to_string("data/day4_example.txt").unwrap();
+        let result = day4b(&data);
+        assert_eq!(result, 43);
+    }
+
+    #[test]
+    fn day4b_real() {
+        let data = std::fs::read_to_string("data/day4.txt").unwrap();
+        let result = day4b(&data);
+        assert_eq!(result, 9397);
+    }
+
 }
